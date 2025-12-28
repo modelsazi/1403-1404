d3fa46790f69a9b2fe7ddd22004ea351f1b266c7
unknown <bardia490@gmail.com>
2025-12-28 11:08:22 +0330

    fixed soal 7_8

--- fasl 7/tamrin7_8.m
+++ fasl 7/tamrin7_8.m
@@ -42,7 +42,7 @@ title("B")
 
 %% C
 f = @(x,y) [y(2)
-            -2/y(1)*y(2)+landa^2*x/y(1)+landa^2*x/y(1)^2];
+            -2/y(1)*y(2)^2+landa^2*x/y(1)+landa^2*x/y(1)^2];
 x_start = 0.1;
 x_end   = L;
 dx = 0.001;
@@ -69,10 +69,10 @@ j = zeros(n,n); B = zeros(n,1);
 y = ones(1,length(x)); err = 1;
 while err > 1e-4 % newton-rafson loop
     for i = 2:n - 1  % calculating coefficients
-        j(i,i-1) = y(i)^2;
-        j(i,i)   = 2*y(i+1)*dx-4*y(i)*dx+2*y(i)*y(i+1)-6*y(i)^2+2*y(i)*y(i-1)-landa^2*x(i)*dx^2;
-        j(i,i+1) = 2*y(i)*dx+y(i)^2;
-        B(i)     = -(2*y(i)*y(i+1)*dx-2*y(i)^2*dx+y(i)^2*y(i+1)-2*y(i)^3+y(i)^2*y(i-1)-landa^2*x(i)*y(i)*dx^2+landa^2*x(i)*dx^2);
+        j(i,i-1) = -2*y(i)*y(i+1)+2*y(i)*y(i-1)+2*y(i)^2;
+        j(i,i)   = y(i+1)^2 -2*y(i+1)*y(i-1)+y(i-1)^2+ 4*y(i)*y(i+1)-12*y(i)^2+4*y(i)*y(i-1)-2*dx^2*landa^2*x(i);
+        j(i,i+1) = 2*y(i)*y(i+1)-2*y(i)*y(i-1)+2*y(i)^2;
+        B(i)     = -(y(i)*y(i+1)^2-2*y(i)*y(i+1)*y(i-1)+y(i)*y(i-1)^2+2*y(i)^2*y(i+1)-4*y(i)^3+2*y(i)^2*y(i-1)-2*dx^2*landa^2*x(i)*y(i)+2*dx^2*landa^2*x(i));
     end
     j(1,1) = -1; j(1,2) = 1; B(1) = -(y(2) - y(1));
     j(n,n)= 1; B(n) = b-y(n);
